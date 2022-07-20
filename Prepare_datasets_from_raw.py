a57cc141b1c9648971668c0e2991c26c6b179c4b
rebeccamarion <rebecca.marion@unamur.be>
2022-07-20 16:56:43 +0200

    added code for pre-processing raw data (Gas); created helper function for formatting data (see utils.py)

--- Prepare_datasets_from_raw.py
+++ Prepare_datasets_from_raw.py
@@ -3,17 +3,24 @@
 Script for transforming data from Data_raw folder into the data in Data folder
 """
 
-import pandas as pd
 import glob 
 import os
+import pandas as pd
+
+# custom functions
 import utils
+
+# Directories for raw data and processed data
+prefix_raw = "Data_raw/"
+prefix_data = "Data/"
 
 ##########################
 #### ELECTRICITY DATA ####
 ##########################
 
-file_path_raw_data = "Data_raw/Electricity/"
-file_path_data = "Data/Electricity/"
+data_type = "Electricity"
+file_path_raw_data = prefix_raw + data_type + "/"
+file_path_data = prefix_data + data_type + "/"
 
 
 ## Meta data (meter ids, building type, etc) ##
@@ -36,26 +43,51 @@ for file_index in range(nb_files):
     
     file_name_consum_data = file_names_consum_data[file_index]
     
-    df = pd.read_csv(file_name_consum_data, sep=" ", header = None)
+    df = pd.read_csv(file_name_consum_data, sep = " ", header = None)
     df.columns = ["ID", "day_time_code", "consumption"]
 
-    #df.memory_usage(deep=True)
-    # reduce memory required for each column
-    df["ID"] = df["ID"].astype("category")
-    df["day_time_code"] = pd.to_numeric(df["day_time_code"], downcast="unsigned")
-    df["consumption"] = pd.to_numeric(df["consumption"], downcast="float")
+    df_sub = utils.format_raw_data(df_old = df, res_IDs = res_IDs)
+    df_list.append(df_sub)
 
-    # add separate columns for day and time codes
-    new_var = df[['day_time_code']].to_numpy().astype('U6').view('U3').astype(int)
-    new_var_df = pd.DataFrame(new_var, columns = ["day_code", "time_code"])
-    df = df.join(new_var_df)
+df_all = pd.concat(df_list).reset_index(drop = True) 
+df_all = df_all.sort_values(['ID', 'day_time_code'], ascending=[True, True]).reset_index(drop = True)
 
-    # reduce memory required for new columns
-    df["day_code"] = pd.to_numeric(df["day_code"], downcast="unsigned")
-    df["time_code"] = pd.to_numeric(df["time_code"], downcast="unsigned")
+file_name_out = file_path_data + "residential_all.pkl"
+utils.save_data(file_name_out, df_all)
 
-    # Keep residential data only
-    df_sub = df.loc[df['ID'].isin(res_IDs), :].reset_index(drop = True) # reset index to avoid increasing memory
+##################
+#### GAS DATA ####
+##################
+
+data_type = "Gas"
+file_path_raw_data = prefix_raw + data_type + "/"
+file_path_data = prefix_data + data_type + "/"
+
+
+## Meta data (meter ids, building type, etc) ##
+
+file_name_meta_data = file_path_raw_data + 'Residential allocations.xls'
+df = pd.read_excel(file_name_meta_data)
+meta_data = df.iloc[:, 0:2]
+# Residential: Code = 1
+res_IDs = list(meta_data.ID) # IDs for residential 
+
+## Consumption data ##
+
+file_names_consum_data = sorted(glob.glob(os.path.join(file_path_raw_data, "*")))[0:-1]
+nb_files = len(file_names_consum_data)
+
+df_list = list()
+for file_index in range(nb_files):
+    
+    print('\n processing file %d of %d' % (file_index + 1, nb_files))
+    
+    file_name_consum_data = file_names_consum_data[file_index]
+    
+    df = pd.read_csv(file_name_consum_data, sep = ",", header = 0)
+    df.columns = ["ID", "day_time_code", "consumption"]
+    
+    df_sub = utils.format_raw_data(df_old = df, res_IDs = res_IDs)
     df_list.append(df_sub)
 
 df_all = pd.concat(df_list).reset_index(drop = True) 
--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ Generic custom functions
 """
 
 import pickle as pkl
+import pandas as pd
 
 def load_data (file) : 
     
@@ -16,3 +17,27 @@ def save_data (file, element) :
     
     with open(file, "bw") as f:
          pkl.dump(element, f)
+         
+def format_raw_data (df_old, res_IDs):
+    
+    df = df_old.copy()
+    
+    #df.memory_usage(deep=True)
+    # reduce memory required for each column
+    df["ID"] = df["ID"].astype("category")
+    df["day_time_code"] = pd.to_numeric(df["day_time_code"], downcast="unsigned")
+    df["consumption"] = pd.to_numeric(df["consumption"], downcast="float")
+
+    # add separate columns for day and time codes
+    new_var = df[['day_time_code']].to_numpy().astype('U6').view('U3').astype(int)
+    new_var_df = pd.DataFrame(new_var, columns = ["day_code", "time_code"])
+    df = df.join(new_var_df)
+
+    # reduce memory required for new columns
+    df["day_code"] = pd.to_numeric(df["day_code"], downcast="unsigned")
+    df["time_code"] = pd.to_numeric(df["time_code"], downcast="unsigned")
+
+    # Keep residential data only
+    df_sub = df.loc[df['ID'].isin(res_IDs), :].reset_index(drop = True) # reset index to avoid increasing memory
+    
+    return df_sub
