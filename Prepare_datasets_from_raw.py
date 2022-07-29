5901a08ba0b04ab6e57ba422df4e64174ec64df5
rebeccamarion <rebecca.marion@unamur.be>
2022-07-29 18:12:59 +0200

    Removed code for processing gas data; updated formatting function to remove data with missing days/times, add date-times, process data for DST days

--- Prepare_datasets_from_raw.py
+++ Prepare_datasets_from_raw.py
@@ -30,6 +30,12 @@ df = pd.read_excel(file_name_meta_data)
 meta_data = df.iloc[:, 0:4]
 # Residential: Code = 1
 res_IDs = list(meta_data.loc[meta_data.Code == 1, "ID"]) # IDs for residential 
+# Daylight savings time (DST) day codes
+dst_days = {"beg": [452],
+            "end": [298, 669]}
+# Daylight savings time (DST) time codes before and after end of DST
+dst_times = {"before": [3, 4],
+            "after": [5, 6]}
 
 ## Consumption data ##
 
@@ -46,48 +52,7 @@ for file_index in range(nb_files):
     df = pd.read_csv(file_name_consum_data, sep = " ", header = None)
     df.columns = ["ID", "day_time_code", "consumption"]
 
-    df_sub = utils.format_raw_data(df_old = df, res_IDs = res_IDs)
-    df_list.append(df_sub)
-
-df_all = pd.concat(df_list).reset_index(drop = True) 
-df_all = df_all.sort_values(['ID', 'day_time_code'], ascending=[True, True]).reset_index(drop = True)
-
-file_name_out = file_path_data + "residential_all.pkl"
-utils.save_data(file_name_out, df_all)
-
-##################
-#### GAS DATA ####
-##################
-
-data_type = "Gas"
-file_path_raw_data = prefix_raw + data_type + "/"
-file_path_data = prefix_data + data_type + "/"
-
-
-## Meta data (meter ids, building type, etc) ##
-
-file_name_meta_data = file_path_raw_data + 'Residential allocations.xls'
-df = pd.read_excel(file_name_meta_data)
-meta_data = df.iloc[:, 0:2]
-# Residential: all IDs
-res_IDs = list(meta_data.ID) # IDs for residential 
-
-## Consumption data ##
-
-file_names_consum_data = sorted(glob.glob(os.path.join(file_path_raw_data, "*")))[0:-1]
-nb_files = len(file_names_consum_data)
-
-df_list = list()
-for file_index in range(nb_files):
-    
-    print('\n processing file %d of %d' % (file_index + 1, nb_files))
-    
-    file_name_consum_data = file_names_consum_data[file_index]
-    
-    df = pd.read_csv(file_name_consum_data, sep = ",", header = 0)
-    df.columns = ["ID", "day_time_code", "consumption"]
-    
-    df_sub = utils.format_raw_data(df_old = df, res_IDs = res_IDs)
+    df_sub = utils.format_raw_data(df_old = df, res_IDs = res_IDs, dst_days = dst_days, dst_times = dst_times)
     df_list.append(df_sub)
 
 df_all = pd.concat(df_list).reset_index(drop = True) 
--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ Custom functions
 
 import pickle as pkl
 import pandas as pd
+import numpy as np
 
 def load_data (file) : 
     """
@@ -44,13 +45,113 @@ def save_data (file, element) :
     with open(file, "bw") as f:
          pkl.dump(element, f)
          
-def format_raw_data (df_old, res_IDs):
+def agg_consumption_dst_hours (x, dst_times):
+    
+    """
+    Aggregates consumption data for the repeated hour at the end of daylight
+    savings time (DST) 
+    
+    Replaces consumption for the hour before DST ends with the mean of consumption 
+    before and after (at the equivalent time points). Removes data for the hour 
+    after DST ends. Shifts the remaining time codes downwards.
+    
+
+    Parameters
+    ----------
+    x : pandas.core.frame.DataFrame
+        Dataset containing the columns "ID", "day_time_code", "consumption", 
+        "day_code" and "time_code" (instances for a single ID and day_code for
+        for a day where DST ends)
+        
+    dst_times : dict
+        time codes related to the repeated hour at the end of daylight savings
+        time (DST), with keys "before" and "after" with values equal to the time
+        codes in the hour before and after DST ends
+
+    Returns
+    -------
+    pandas.core.frame.DataFrame
+        Dataset without the extra hour resulting from the end of DST
+        
+    """
+    
+    # Sort dataset by time_code
+    x = x.sort_values(['time_code'], ascending=[True]).reset_index(drop = True)
+    
+    # Identify indexes for the hour before and after DST ends
+    wh_before = x.time_code.isin(dst_times["before"])
+    wh_after = x.time_code.isin(dst_times["after"])
+    
+    # Aggregate the consumption levels (before and after, mean)
+    consumption_vals = np.array([x.loc[wh_before, "consumption"], x.loc[wh_after, "consumption"]])
+    mean_consumption_vals = np.mean(consumption_vals, axis = 0)
+    
+    # Update the consumptions values for the hour before DST ends
+    x.loc[wh_before, "consumption"] = mean_consumption_vals
+    
+    # Remove the rows for the hour after DST ends
+    x = x.loc[np.logical_not(wh_after), :]
+    
+    # Shift the remaining time codes downwards
+    wh_shift = x.time_code > np.max(dst_times["after"])
+    x.loc[wh_shift, "time_code"] = x.loc[wh_shift, "time_code"] - len(dst_times["after"])
+    
+    return x
+
+def interpolate_consumption_dst_hours(x):
+    """
+    Adds rows for missing time codes and interpolates missing consumption values  
+
+    Parameters
+    ----------
+    x : pandas.core.frame.DataFrame
+        Dataset containing the columns "ID", "day_time_code", "consumption", 
+        "day_code" and "time_code" (instances for a single ID and day_code for
+        for a day where DST begins)
+
+    Returns
+    -------
+    pandas.core.frame.DataFrame
+        Dataset with added rows for missing time codes (consumption interpolated)
+        
+    """
+    # Sort dataset by time_code
+    x = x.sort_values(['time_code'], ascending=[True]).reset_index(drop = True)
+    
+    # Missing time codes
+    time_code_min = x.time_code.min()
+    time_code_max = x.time_code.max()
+    all_time_codes = set(np.arange(time_code_min, time_code_max + 1))
+    observed_time_codes = set(x.time_code)
+    missing_time_codes = list(all_time_codes.difference(observed_time_codes))
+    
+    # Create rows for missing time codes
+    x_missing = x.iloc[range(len(missing_time_codes)), :].copy()
+    x_missing.consumption = np.nan
+    x_missing.time_code = missing_time_codes
+    day_code_str = x_missing.day_code.astype(str).apply(lambda y: y.zfill(3))
+    time_code_str = x_missing.time_code.astype(str).apply(lambda y: y.zfill(2))
+    x_missing.day_time_code = (day_code_str + time_code_str).astype(int)
+    
+    # Add missing rows to x
+    x = pd.concat([x, x_missing]).sort_values(['time_code'], ascending=[True]).reset_index(drop = True)
+    
+    # Interpolate missing consumption values
+    x.consumption = x.consumption.interpolate()
+    
+    return x
+    
+         
+def format_raw_data (df_old, res_IDs, dst_days, dst_times):
     """
     Formats raw data imported from ISSDA .csv and .txt files
     
     Column dtypes are downcast to lower-memory versions; new columns for day 
     and time codes are generated based on the column "day_time_code"; 
-    non-residential instances are removed.
+    non-residential instances are removed; days where DST begins and ends are
+    processed using the functions interpolate_consumption_dst_hours() and 
+    agg_consumption_dst_hours(); instances (meters) with missing days/times
+    are removed.
 
     Parameters
     ----------
@@ -59,6 +160,15 @@ def format_raw_data (df_old, res_IDs):
         
     res_IDs : list
         ID numbers for instances classified as "residential" consumers
+    
+    dst_days : dict
+        day codes related to daylight savings time (DST), with keys "end" for 
+        day codes indicating the end of DST and "beg" for the beginning of DST
+        
+    dst_times : dict
+        time codes related to the repeated hour at the end of daylight savings
+        time (DST), with keys "before" and "after" with values equal to the time
+        codes in the hour before and after DST ends
 
     Returns
     -------
@@ -66,27 +176,62 @@ def format_raw_data (df_old, res_IDs):
         Dataset containing only residential data, with columns "ID", 
         "day_time_code", "consumption", "day_code" and "time_code". For more 
         information on the codes used for "day_time_code", etc., visit
-        https://www.ucd.ie/issda/data/commissionforenergyregulationcer/ .
+        https://www.ucd.ie/issda/data/commissionforenergyregulationcer/ . 
+        
     """
     
     df = df_old.copy()
     
+    # Keep residential data only
+    df = df.loc[df['ID'].isin(res_IDs), :].reset_index(drop = True) # reset index to avoid increasing memory
+    
     #df.memory_usage(deep=True)
-    # reduce memory required for each column
+    # Reduce memory required for each column
     df["ID"] = df["ID"].astype("category")
     df["day_time_code"] = pd.to_numeric(df["day_time_code"], downcast="unsigned")
     df["consumption"] = pd.to_numeric(df["consumption"], downcast="float")
 
-    # add separate columns for day and time codes
+    # Add separate columns for day and time codes
     new_var = df[['day_time_code']].to_numpy().astype('U6').view('U3').astype(int)
     new_var_df = pd.DataFrame(new_var, columns = ["day_code", "time_code"])
     df = df.join(new_var_df)
 
-    # reduce memory required for new columns
+    # Reduce memory required for new columns
     df["day_code"] = pd.to_numeric(df["day_code"], downcast="unsigned")
     df["time_code"] = pd.to_numeric(df["time_code"], downcast="unsigned")
-
-    # Keep residential data only
-    df_sub = df.loc[df['ID'].isin(res_IDs), :].reset_index(drop = True) # reset index to avoid increasing memory
     
-    return df_sub
+    # Check for missing day/times for each ID
+    day_time_count_df = df.groupby("ID", as_index = False).apply(lambda x: x.day_time_code.nunique())
+    day_time_count_df = day_time_count_df.rename(columns = {None: "counts"})
+    # Remove instances with missing days and/or times
+    max_count = day_time_count_df.loc[:, "counts"].max()
+    IDs_without_missing = set(day_time_count_df.loc[day_time_count_df.counts == max_count, "ID"])
+    df = df.loc[df['ID'].isin(IDs_without_missing), :].reset_index(drop = True)
+    
+    # Split data into non-DST days and DST-end and DST-beg days
+    all_dst_days = [item for sublist in dst_days.values() for item in sublist]
+    df_non_dst = df.loc[np.logical_not(df.day_code.isin(all_dst_days)), :]
+    df_dst_end = df.loc[df.day_code.isin(dst_days["end"]), :]
+    df_dst_beg = df.loc[df.day_code.isin(dst_days["beg"]), :]
+    
+    # Aggregate data for repeated hour on days where DST ends 
+    grouped_dst_end = df_dst_end.groupby(["ID", "day_code"], as_index = False)
+    df_dst_end = grouped_dst_end.apply(lambda x: agg_consumption_dst_hours(x, dst_times)).reset_index(drop = True)
+
+    # Add rows for lost hour on days where DST begins and interpolate missing values
+    grouped_dst_beg = df_dst_beg.groupby(["ID", "day_code"], as_index = False)
+    df_dst_beg = grouped_dst_beg.apply(lambda x: interpolate_consumption_dst_hours(x)).reset_index(drop = True)
+    
+    # Concatenate all sub dfs
+    df = pd.concat([df_non_dst, df_dst_end, df_dst_beg]).reset_index(drop = True)
+   
+    # Transform day_code to date
+    df["date"] = pd.to_datetime(df["day_code"], unit='D', origin='2008-12-31')
+    
+    # Tranform time_code to time
+    df["time"] = pd.TimedeltaIndex(df['time_code']*30, unit='m')
+    
+    # Create date-time timestamp column
+    df["date_time"] = df["date"] + df["time"] 
+    
+    return df
